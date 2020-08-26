d0be5aec3998a58f4e257cd4b8aff39ec7c85926
Alexander Gauggel <agauggel@uni-koblenz.de>
2020-08-26 16:24:46 +0200

    removed superfluos comment line

--- Plain/src/Rendering/RenderFrontend.h
+++ Plain/src/Rendering/RenderFrontend.h
@@ -53,7 +53,6 @@ public:
 
 private:
 
-    //returns image from file
     //checks a map of all loaded images if it is avaible, returns existing image if possible    
     bool loadImageFromPath(std::filesystem::path path, ImageHandle* outImageHandle);
     std::map<std::filesystem::path, ImageHandle> m_textureMap;
