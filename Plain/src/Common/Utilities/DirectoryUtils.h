8340db25dff99fba602d8872d0edc46d7d6fb356
Alexander Gauggel <agauggel@uni-koblenz.de>
2020-10-14 15:07:44 +0200

    comment style update

--- Plain/src/Common/Utilities/DirectoryUtils.h
+++ Plain/src/Common/Utilities/DirectoryUtils.h
@@ -11,10 +11,8 @@ public:
     static fs::path getResourceDirectory();
 
 private:
-	/*
-    loops trough up from working directory and checks for "resource" folder
-	if none is found returns root of the path (C: or similar)
-    */
+    //loops trough up from working directory and checks for "resource" folder
+	//if none is found returns root of the path (C: or similar)
 	static fs::path searchResourceDirectory(const fs::path& workingDirectory);
 
 	static fs::path m_workingDirectory;
