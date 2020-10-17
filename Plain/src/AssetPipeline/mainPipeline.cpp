2efd57c1ccc6eaeb736738910f2cebe5919e61f8
Alexander Gauggel <agauggel@uni-koblenz.de>
2020-10-17 17:34:04 +0200

    asset pipeline input file now set trough command line

--- Plain/src/AssetPipeline/mainPipeline.cpp
+++ Plain/src/AssetPipeline/mainPipeline.cpp
@@ -4,17 +4,38 @@
 #include "Common/MeshProcessing.h"
 #include "Utilities/DirectoryUtils.h"
 
-int main() {
+//expected command line arguments:
+//argv[0] = executablePath
+//argv[1] = .obj scene file path
+struct CommandLineSettings {
+    std::string modelFilePath;
+};
+
+CommandLineSettings parseCommandLineArguments(const int argc, char* argv[]) {
+    std::filesystem::path executablePath = argv[0];
+    
+    CommandLineSettings settings;
+    if (argc < 2) {
+        std::cout << "Missing command line parameter, scene file path not set\n";
+    }
+    settings.modelFilePath = argv[1];
+    return settings;
+}
+
+int main(const int argc, char* argv[]) {
+
+    CommandLineSettings settings = parseCommandLineArguments(argc, argv);
 
     DirectoryUtils::init();
-    const std::string inputFile = "Models\\Sponza\\Sponza.obj";
-    const std::string outputFile = "Models\\Sponza\\Sponza.bin";
+    
+    std::filesystem::path outputFile =  settings.modelFilePath;
+    outputFile.replace_extension("bin");
 
     std::vector<MeshData> meshData;
-    if (loadModelOBJ(inputFile, &meshData)) {
+    if (loadModelOBJ(settings.modelFilePath, &meshData)) {
         std::vector<MeshBinary> meshesBinary = meshesToBinary(meshData);
         saveBinaryMeshData(outputFile, meshesBinary);
-        std::cout << "Input model: " << inputFile << "\n";
+        std::cout << "Input model: " << settings.modelFilePath << "\n";
         std::cout << "Sucessfully converted model to binary format\n";
         std::cout << "Saved binary file: " << outputFile << "\n";
     }
