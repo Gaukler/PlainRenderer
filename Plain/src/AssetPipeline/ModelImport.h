073e1954353212ce2f453e42632dea0ef70e3d52
Alexander Gauggel <agauggel@uni-koblenz.de>
2021-02-03 16:41:47 +0100

    removed .obj model loading and corresponding mesh processing functions to avoid maintenance of unused code

--- Plain/src/AssetPipeline/ModelImport.h
+++ Plain/src/AssetPipeline/ModelImport.h
@@ -3,7 +3,4 @@
 #include "Common/MeshData.h"
 #include "Scene.h"
 
-//filename is relative to resource directory
-bool loadModelOBJ(const std::filesystem::path& filename, std::vector<MeshData>* outData);
-
 bool loadModelGLTF(const std::filesystem::path& filename, Scene* outScene);
--- Plain/src/Common/MeshProcessing.h
+++ Plain/src/Common/MeshProcessing.h
@@ -3,6 +3,4 @@
 #include "Common/MeshData.h"
 
 std::vector<AxisAlignedBoundingBox> AABBListFromMeshes(const std::vector<MeshData>& meshes);
-void computeTangentBitangent(MeshData* outMeshData);
-MeshData buildIndexedData(const MeshData& rawData);
 std::vector<MeshBinary> meshesToBinary(const std::vector<MeshData>& meshes, const std::vector<AxisAlignedBoundingBox>& AABBList);
