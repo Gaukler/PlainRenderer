413477315ea7e928890ead52e24ab925bc110ef4
Alexander Gauggel <agauggel@uni-koblenz.de>
2021-03-19 16:19:43 +0100

    made utility function input const

--- Plain/src/Common/Utilities/GeneralUtils.cpp
+++ Plain/src/Common/Utilities/GeneralUtils.cpp
@@ -5,7 +5,7 @@ bool vectorContains(const std::vector<uint32_t>& vector, const uint32_t index) {
     return std::find(vector.begin(), vector.end(), index) != vector.end();
 }
 
-std::vector<char> dataToCharArray(void* data, const size_t size) {
+std::vector<char> dataToCharArray(const void* data, const size_t size) {
     std::vector<char> result(size);
     memcpy(result.data(), data, size);
     return result;
--- Plain/src/Common/Utilities/GeneralUtils.h
+++ Plain/src/Common/Utilities/GeneralUtils.h
@@ -4,4 +4,4 @@
 bool vectorContains(const std::vector<uint32_t>& vector, const uint32_t pass);
 
 //copies arbitrary data to a char vector
-std::vector<char> dataToCharArray(void* data, const size_t size);
+std::vector<char> dataToCharArray(const void* data, const size_t size);
