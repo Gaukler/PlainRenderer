4ca8fa13ee06251e311d4282d1bb924ed8c0ac93
Alexander Gauggel <agauggel@uni-koblenz.de>
2021-05-11 18:04:43 +0200

    deleted dead files that should not have been commited

--- Plain/src/Runtime/Rendering/Backend/Buffer.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "pch.h"
-#include "Resources.h"
-
-Buffer createVulkanBuffer(const VkDeviceSize size, const std::vector<uint32_t>& queueFamilies, 
-    const VkBufferUsageFlags usage, const uint32_t memoryFlags);
-
-void destroyVulkanBuffer(const Buffer& buffer);
--- Plain/src/Runtime/Rendering/Backend/Sampler.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <vulkan/vulkan.h>
-#include "../ResourceDescriptions.h"
-
-VkSampler createVulkanSampler(const SamplerDescription& desc);
